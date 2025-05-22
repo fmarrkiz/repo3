3a60303e172072eef536a85af0c7bbfaa5b8bd93
Marie KF <mariekfruchart@outlook.com>
2025-05-22 13:26:59 +0100

    (main.js and index.html)removed unecessary inputs and lines)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 "use strict";
 
-let ShowScoreInput = document.querySelectorAll(".ScoreInput");
-let ScoreDisplay = document.querySelectorAll(".ScoreDisplay");
-let player1ScoreSpan = document.querySelector("#player1Score");
-let player2ScoreSpan = document.querySelector("#player2Score");
-let ScoringButtons = document.querySelectorAll(".ScoringButton");
+//let ShowScoreInput = document.querySelectorAll(".ScoreInput");
+
+let player1ScoreDisplay = document.querySelector("#player1Score");
+let player2ScoreDisplay = document.querySelector("#player2Score");
 let Player1button = document.querySelector("#player1Button");
 let Player2Button = document.querySelector("#player2Button");
 
@@ -12,28 +11,52 @@ let scorePlayer1 = 0;
 let scorePlayer2 = 0;
 let maxScore = 10;
 let gameEnded = false;
+let resetButton = document.querySelector("#resetButton");
 
-ScoringButtons.appendChild(ScoringPlayer1);
-ScoringButtons.appendChild(ScoringPlayer2);
-
-
-
+Player1button.addEventListener("click", AddPointsPlayerOne);
+Player2Button.addEventListener("click", AddPointsPlayerTwo);
 
 
 function AddPointsPlayerOne(){
     if (!gameEnded) {
-        scorePlayer1++;
+            scorePlayer1++;
+            ShowScore();
+            AndTheWinnerIs();
+    }
+}
+
+function AddPointsPlayerTwo() {
+    if (!gameEnded) {
+        scorePlayer2++;
         ShowScore();
         AndTheWinnerIs();
     }
 }
 
-function ShowScore();
+function AndTheWinnerIs(){
+    if (maxScore === scorePlayer1) {
+        gameEnded = true;
+        alert("Player 1 wins!");
+    }
+    else if (maxScore === scorePlayer2) {
+        gameEnded = true;
+        alert("Player 2 wins!");
+    }
 
-function AndTheWinnerIs();
-
-function VictoryMessage(){
-    if 
 };
 
-function ResetGame();
+
+function ShowScore() {
+    player1ScoreDisplay.textContent = scorePlayer1;
+    player2ScoreDisplay.textContent = scorePlayer2;
+}
+
+resetButton.addEventListener("click", ResetGame);
+
+function ResetGame(){
+    scorePlayer1 = 0;
+    scorePlayer2 = 0;
+    gameEnded = false;
+    ShowScore();
+
+}
