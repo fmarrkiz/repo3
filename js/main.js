98f6095131a1b62effefa4c2cb09df7c4b137b6f
Marie KF <mariekfruchart@outlook.com>
2025-05-22 17:43:23 +0100

    (index.html)fixed the link with the js file

--- js/main.js
+++ js/main.js
@@ -6,20 +6,33 @@ let player1ScoreDisplay = document.querySelector("#player1Score");
 let player2ScoreDisplay = document.querySelector("#player2Score");
 let Player1button = document.querySelector("#player1Button");
 let Player2Button = document.querySelector("#player2Button");
+let resetButton = document.querySelector(".ResetButton");
+
 
 let scorePlayer1 = 0;
 let scorePlayer2 = 0;
 let maxScore = 10;
 let gameEnded = false;
-let resetButton = document.querySelector("#resetButton");
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    scorePlayer1 = 0;
+    scorePlayer2 = 0;
+
+
 
 Player1button.addEventListener("click", AddPointsPlayerOne);
 Player2Button.addEventListener("click", AddPointsPlayerTwo);
+resetButton.addEventListener("click", ResetGame);
 
+
+});
 
 function AddPointsPlayerOne(){
+    console.log("Player 1 button clicked!");
     if (!gameEnded) {
             scorePlayer1++;
+            console.log("Player 1 score:", scorePlayer1);
             ShowScore();
             AndTheWinnerIs();
     }
@@ -33,30 +46,42 @@ function AddPointsPlayerTwo() {
     }
 }
 
+function ShowScore() {
+    player1ScoreDisplay.innerText = scorePlayer1;
+    player2ScoreDisplay.innerText = scorePlayer2;
+}
+
 function AndTheWinnerIs(){
     if (maxScore === scorePlayer1) {
         gameEnded = true;
         alert("Player 1 wins!");
+        DisableButtons;
     }
     else if (maxScore === scorePlayer2) {
         gameEnded = true;
         alert("Player 2 wins!");
-    }
+        DisableButtons;
+        }
 
 };
 
 
-function ShowScore() {
-    player1ScoreDisplay.textContent = scorePlayer1;
-    player2ScoreDisplay.textContent = scorePlayer2;
+function DisableButtons() {
+    Player1button.disabled = true;
+    Player2Button.disabled = true;
 }
 
-resetButton.addEventListener("click", ResetGame);
+function EnableButtons() {
+    Player1button.disabled = false;
+    Player2Button.disabled = false;
+}
+
 
 function ResetGame(){
     scorePlayer1 = 0;
     scorePlayer2 = 0;
     gameEnded = false;
     ShowScore();
+    EnableButtons();
 
 }
